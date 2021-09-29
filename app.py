36155f33c5ea49b080738e896acc9db6a21f62c2
Pap Gergely <91294375+yrogerg0@users.noreply.github.com>
2021-09-29 10:56:11 +0200

    Update app.py
    A "little" makeover to the program.

--- app.py
+++ app.py
@@ -1,76 +1,117 @@
-# this program will do a mathematical operation, that is given by the user, on 2 given numbers, which is also given by the user.
+from os import system
 from time import sleep
-import os
+import platform
 
-def clear():
-    os.system("cls")
+# i've made a function for everything for the simplicity & the ability
+# to jump back & forth, wherever I want the program to jump to.
 
-def exit(n):
-    while n != 0:
+
+# checks what platform you opened the program from
+# which is useful so I can clear your terminal / cmd prompt.
+if platform.system() == "Windows":
+    # pre-defining the clear function
+    def clear():
+        system("cls")
+else:
+    # pre-defining the clear function
+    def clear():
+        system("clear")
+
+# pre-defining the exit function
+def exit(i):
+    while i != 0:
         clear()
-        print("The program will exit in " + str(n) + " seconds")
+        print("The program will exit in "+str(int(i))+" seconds.")
         sleep(1)
+        i -= 1
+    if i == 0:
         clear()
-        n -= 1
 
+# main program
 def calculator():
-    # input of the first number
-    print("Type in a number")
-    num1 = input("Input: ")
-    print()
-
-    # input of the mathematical operator
+    
     clear()
-    print("Your equation: " + num1)
-    print("Type in the mathematical operation (\"+\",\"-\",\"*\",\"/\")")
-    operator = input("Input: ")
-    print()
+    
+    # first number input
+    def num1_input():
+        try:
+            num1_input.num1 = float(input("num1: "))
+        except ValueError:
+            clear()
+            print("Invalid number, try again!")
+            num1_input()
+        except AttributeError:
+            clear()
+            print("Invalid number, try again!")
+            num1_input()
+    num1_input()
+    num1 = num1_input.num1
 
-    # input of the second number
     clear()
-    print("Your equation: "+ num1+operator)
-    print("Type in another number")
-    num2 = input("Input: ")
-    print()
 
-    # output of the answer
+    # operator input
+    def operator_input():
+        operator_input.operator = input("[+] = addition\n[-] = substraction\n[*] = multiplication\n[/] = division\noperator: ")
+        operators = ["/","*","+","-"]
+        if operator_input.operator not in operators:
+            clear()
+            print("Incorrect operator, try again!")
+            operator_input()
+    operator_input()
+    operator = operator_input.operator
+
     clear()
-    print("Your equation is: "+num1+operator+num2)
-    num1 = float(num1)
-    num2 = float(num2)
-    if num1 % 1 == 0:
-        num1 == int(num1)
-    else:
-        num1 == float(num1)
 
-    if num2 % 1 == 0:
-        num2 == int(num2)
-    else:
-        num2 == float(num2)
+    # second number input
+    def num2_input():
+        try:
+            num2_input.num2 = float(input("num2: "))
+        except ValueError:
+            clear()
+            print("Invalid number, try again!")
+            num2_input()
+    num2_input()
+    num2 = num2_input.num2
 
-    if operator == "+":
-        answer = num1+num2
-    elif operator == "-":
-        answer = num1-num2
-    elif operator == "*":
-        answer = num1*num2
-    elif operator == "/":
-        answer = num1/num2
+    # elif operators
+    def operator_choose():
+        if operator == "+":
+            operator_choose.answer = num1 + num2
+        elif operator == "-":
+            operator_choose.answer = num1 - num2
+        elif operator == "*":
+            operator_choose.answer = num1 * num2
+        elif operator == "/":
+            operator_choose.answer = num1 / num2
+    operator_choose()
+    final_answer = operator_choose.answer
 
-    if answer % 1 == 0:
-        print("Which equals to: "+str(int(answer)))
-    else:
-        print("Which equals to: "+str(answer))
+    # rounding the answer if it's an integer.
+    def answer_round():
+        if final_answer % 1 == 0:
+            answer_round.answer = int(final_answer)
+        else:
+            answer_round.answer = float(final_answer)
+    answer_round()
+    answer = answer_round.answer
 
-    yesList = ["y","Y"]
-    noList = ["n","N"]
-    con = input("Do you want to make another calculation? (Y,N)\n")
-
-    if con in yesList:
+    # printing the answer
+    def answer_prompt():
         clear()
-        calculator()
-    elif con in noList:
-        exit(3)
-    else:
-        print("You managed to say neither of the choices.")
+        print("answer: "+str(answer))
+    answer_prompt()
+
+    # restart program or continue
+    def con():
+        var = input("[1] = new calculation\n[2] = quit game\nYou: ")
+        if var == "1":
+            calculator()
+        if var == "2":
+            exit(3)
+        else:
+            clear()
+            answer_prompt()
+            print("Wrong input, try again!")
+            con()
+    con()
 calculator()
